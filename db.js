235c448f78b5e3cc1cd64891ea94a29627d03b98
casualshay <akshaysprabhakar@gmail.com>
2019-10-02 13:32:10 -0700

    progress...

--- db.js
+++ db.js
@@ -1,28 +1,33 @@
 const Sequelize = require('sequelize');
 const { STRING, UUID, UUIDV4 } = Sequelize;
 
-const conn = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost/junior_phase_final_project_acme_school');
+const conn = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost/acme_cms');
 
-const Page = db.define('Page', {
+const Page = conn.define('Page', {
   title: {
-    type: Sequelize.STRING
+    type: STRING
   },
-  parentId
+  parentId: {
+    type: UUID,
+    primaryKey: true,
+    defaultValue: UUIDV4,
+  }
 });
 
-// Title.belongsTo(Page);
-// Page.hasMany(Titles);
+Page.hasMany(Page, {as: 'parent'});
 
 const mapAndSave = (pages) => Promise.all(pages.map( page => Page.create(page)));
 
 const syncAndSeed = async () => {
   await conn.sync({ force: true });
+
   const home = await Page.create({ title: 'Home Page' });
   let pages = [
     { title: 'About', parentId: home.id },
     { title: 'Contact', parentId: home.id }
   ];
   const [ about, contact ] = await mapAndSave(pages);
+
   pages = [
     { title: 'About Our Team', parentId: about.id },
     { title: 'About Our History', parentId: about.id },
@@ -32,18 +37,28 @@ const syncAndSeed = async () => {
   const [ team, history, phone, fax ] = await mapAndSave(pages);
 };
 
+Page.findHomePage = function () {
+  console.log(Page)
+  return Page.findAll();
+}
+
 syncAndSeed()
   .then( async () => {
     const home = await Page.findHomePage();
     console.log(home.title);//Home Page
-    const homeChildren = await home.findChildren();
-    console.log(homeChildren.map( page => page.title ));//[About, Contact];
-    const fax = await Page.findOne({ where: {title: 'Fax'} });
-    console.log(fax.title);
-    //hierarch returns the page, parentPage, parent's Parent.. etc..
-    let hier = await fax.hierarchy();
-    console.log(hier.map( page => page.title ));//['Fax', 'Contact', 'Home']
-    const history = await Page.findOne({ where : { title: 'About Our History'} });
-    hier = await history.hierarchy();
-    console.log(hier.map( page => page.title));//[ 'About Our History', 'About', 'Home Page' ]
-  })
+    // const homeChildren = await home.findChildren();
+    // console.log(homeChildren.map( page => page.title ));//[About, Contact];
+    // const fax = await Page.findOne({ where: {title: 'Fax'} });
+    // console.log(fax.title);
+    // //hierarch returns the page, parentPage, parent's Parent.. etc..
+    // let hier = await fax.hierarchy();
+    // console.log(hier.map( page => page.title ));//['Fax', 'Contact', 'Home']
+    // const history = await Page.findOne({ where : { title: 'About Our History'} });
+    // hier = await history.hierarchy();
+    // console.log(hier.map( page => page.title));//[ 'About Our History', 'About', 'Home Page' ]
+  });
+
+module.exports = {
+  syncAndSeed,
+  Page
+}
--- server.js
+++ server.js
@@ -2,10 +2,16 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const db = require('./db');
+const { Page } = require('./db');
 const port = process.env.PORT || 3000;
 
-app.get('/api/pages', async () => {
-
+app.get('/api/pages', async ( req, res, next ) => {
+  try {
+    res.send(await Page.findAll());
+  }
+  catch(ex) {
+    next(ex);
+  }
 });
 
 app.get('/api/pages/:id/children', async () => {
@@ -15,3 +21,8 @@ app.get('/api/pages/:id/children', async () => {
 app.get('/api/pages/:id/siblings', async () => {
   
 });
+
+db.syncAndSeed()
+  .then(() => {
+  app.listen(port, () => console.log(`listening on port ${port}`));
+})
